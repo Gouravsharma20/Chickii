7541366c6c5825da969c44a17719c70db7e37aaf
Gourav <gouravsharma20a@gmail.com>
2024-07-02 15:59:56 +0530

    sweet mai daal dii dono patti

---
+++ pattis/src/sweets/gurpatti.js
@@ -0,0 +1,8 @@
+function gurpatti(){
+    return(
+        <div>
+            <h1>ye gur kii hoti hai</h1>
+        </div>
+    );
+}
+export default gurpatti;
---
+++ pattis/src/sweets/rabri.jsx
@@ -0,0 +1,8 @@
+function Rabdi(){
+    return (
+        <div>
+            <h1>ye achii nii hoti</h1>
+        </div>
+    );
+}
+export default Rabdi;
